fb92810e8763d3ca3528dc3c8f39e95ce16cbddb
eunyoung23 <eun970923@naver.com>
2024-05-15 16:23:37 +0900

    -modify index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+const express = require("express"); //express를 설치했기 때문에 가져올 수 있다.
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(8000)
+app.listen(8000, () => {
+    console.log("Running express server");
+});
